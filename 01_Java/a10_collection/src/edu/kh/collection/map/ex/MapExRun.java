04a473045996992e2dac0958b07e455c4b0031d7
dlwlstjd <dlwlstjd4321@naver.com>
2023-08-10 12:31:30 +0900

    0810

---
+++ 01_Java/a10_collection/src/edu/kh/collection/map/ex/MapExRun.java
@@ -0,0 +1,10 @@
+package edu.kh.collection.map.ex;
+
+public class MapExRun {
+	public static void main(String[] args) {
+		
+		MapEx ex = new MapEx();
+		ex.ex3();
+	}
+
+}
---
+++ 01_Java/a10_collection/src/edu/kh/collection/set/ex/SetExRun.java
@@ -0,0 +1,9 @@
+package edu.kh.collection.set.ex;
+
+public class SetExRun {
+	public static void main(String[] args) {
+		SetEx ex = new SetEx();
+		ex.lottoVersion2();
+	}
+
+}
